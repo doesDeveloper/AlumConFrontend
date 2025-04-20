38b9e648745b01a110aa58fde5e9dd7491f2af8c
doesDeveloper <ahmadumarusmanixa1@gmail.com>
2025-04-20 09:47:03 +0530

    fix homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
 	});
 </script>
 
-<h1>Welcome to Alumnicon</h1>
-<nav>
+<h1>Redirecting.....</h1>
+<!-- <nav>
 	<a href="{base}/login">Login</a>
 	<a href="{base}/register">Register</a>
 	<a href="{base}/feed">feed me</a>
 	<a href="{base}/alumnis">Alumni</a>
 	<a href="{base}/profile">Profile</a>
-</nav>
+</nav> -->
