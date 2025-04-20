3bf612041894aafb9ef047fe985cb7dd7aea8357
doesDeveloper <ahmadumarusmanixa1@gmail.com>
2025-04-20 19:27:20 +0530

    for bois

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,3 @@
 // src/lib/config.js
-// export const API_URL = 'https://alumconbackend.onrender.com/api/';
-export const API_URL = 'http://localhost:8080/api/';
+export const API_URL = 'https://alumconbackend.onrender.com/api/';
+// export const API_URL = 'http://localhost:8080/api/';
