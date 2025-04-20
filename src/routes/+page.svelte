69736a6d2c9b2b4c46427e91048bd491f0cbf513
doesDeveloper <ahmadumarusmanixa1@gmail.com>
2025-04-20 09:36:16 +0530

    Oh God its working ig

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		goto(base + '/home');
+	});
 </script>
 
 <h1>Welcome to Alumnicon</h1>
